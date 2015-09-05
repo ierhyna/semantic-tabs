ac03db42785776ec93c5d74c0d07ab5dc1538d07
Irina Sokolovskaja <sokolovskaja.irina@gmail.com>
2015-09-05 17:22:13 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Semantic Tabs with Pure JavaScript
+
+[Demo](http://jsfiddle.net/ierhyna/c9hwgdyz/show)
+
+The HTML structure is the following:
+
+`
+<div class="tabs">
+    <div class="tab">
+        <h3 class="tab-header">Tab 1</h3>
+        <div class="tab-text"></div>
+    </div>
+    ...
+    <div class="tab">
+        <h3 class="tab-header">Tab 5</h3>
+        <div class="tab-text"></div>
+    </div>
+</div>
+`
---
+++ script.js
@@ -0,0 +1,34 @@
+/*
+Semantic Tabs with pure JS
+Author: Irina Sokolovskaya (http://github.com/ierhyna)
+*/
+
+function changeTabs() {
+
+    var tabHeader = document.getElementsByClassName('tab-header');
+    var tabText = document.getElementsByClassName('tab-text');
+
+    // do the first tab active
+    tabHeader[0].classList.add('active');
+    tabText[0].style.display = 'block';
+
+    for (var i = 0; i < tabHeader.length; i++) {
+
+        tabHeader[i].addEventListener( 'click', function() {
+
+            for (var i = 0; i < tabHeader.length; i++) {
+                tabHeader[i].classList.remove('active');
+            };
+
+            for (var i = 0; i < tabText.length; i++) {
+                tabText[i].style.display = 'none';
+            };
+
+            this.nextElementSibling.style.display = 'block';
+            this.classList.add('active');
+
+        });
+    }
+}
+
+changeTabs();
